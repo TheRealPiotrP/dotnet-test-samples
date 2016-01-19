8d0e01970824fe089bafca8db2654135323437d3
piotrp <piotrp@microsoft.com>
2016-01-18 21:24:11 -0800

    Initial commit of sample tests

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace SampleTests
+{
+    public class Given_some_tests
+    {
+        [Fact]
+        public void When_a_test_passes_it_is_reported_as_passing()
+        {
+            Assert.Equal(true, true);
+        }
+        
+        [Fact]
+        public void When_a_test_fails_it_is_reported_as_failing()
+        {
+            Assert.Equal(true, false);
+        }
+    }
+}
---
+++ buildAndTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dotnet restore
+dotnet build
+dotnet test
